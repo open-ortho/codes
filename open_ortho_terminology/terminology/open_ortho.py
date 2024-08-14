17c7dbea0fd939c08f2d57d97fc55d3ed68b359a
Toni Magni <email@antoniomagni.com>
2024-08-14 23:10:33 +0200

    Add VS01.

--- open_ortho_terminology/terminology/open_ortho.py
+++ open_ortho_terminology/terminology/open_ortho.py
@@ -2,7 +2,8 @@
 
 Used whenever a code is necessary, for various implementations.
 """
-from . import Code
+from open_ortho_terminology.terminology import Code
+from open_ortho_terminology.terminology.snomed import EV01, EV15, EV19
 PREFIX = 'OPOR'
 
 
@@ -25,7 +26,7 @@ def make_code(s):
     return ''.join(str(ord(char)) for char in s)
 
 
-class CodeOpenOrtho(Code):
+class OpenOrthoCode(Code):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -33,196 +34,215 @@ class CodeOpenOrtho(Code):
         self.system = NAMESPACES.url
 
 
-IV01 = CodeOpenOrtho(
+EV07 = OpenOrthoCode(
+    code=f"{make_code('EV07')}",
+    display="Extraoral, Right Profile (subject is facing observer's right), Mandible Postured Forward",
+    synonyms=['EV-07', 'EO.RP.MD.PF'])
+""" Used for ... """
+
+EV28 = OpenOrthoCode(
+    code=f"{make_code('EV28')}",
+    display="Extraoral, Left Profile (subject is facing observer's left), Mandible Postured Forward",
+    synonyms=['EV-28', 'EO.LP.MD.PF'])
+""" Used for ... """
+
+IV01 = OpenOrthoCode(
     code=f"{make_code('IV01')}",
     display='Intraoral Right Buccal Segment, Centric Occlusion, Direct View',
-    synonyms=['IV-01','IO.RB.CO'])
+    synonyms=['IV-01', 'IO.RB.CO'])
 """ Used for ... """
 
-IV02 = CodeOpenOrtho(
+IV02 = OpenOrthoCode(
     code=f"{make_code('IV02')}",
     display='Intraoral, Right Buccal Segment, Centric Occlusion, With Mirror',
-    synonyms=['IV-02','IO.RB.CO.WM'])
+    synonyms=['IV-02', 'IO.RB.CO.WM'])
 """ Used for ... """
 
-IV03 = CodeOpenOrtho(
+IV03 = OpenOrthoCode(
     code=f"{make_code('IV03')}",
     display='Intraoral, Right Buccal Segment, Centric Occlusion, With Mirror, But Corrected',
-    synonyms=['IV-03','IO.RB.CO.WM.BC'])
+    synonyms=['IV-03', 'IO.RB.CO.WM.BC'])
 """ Used for ... """
 
-IV04 = CodeOpenOrtho(
+IV04 = OpenOrthoCode(
     code=f"{make_code('IV04')}",
     display='Intraoral, Right Buccal Segment, Centric Relation (Direct View)',
-    synonyms=['IV-04','IO.RB.CR'])
+    synonyms=['IV-04', 'IO.RB.CR'])
 """ Used for ... """
 
-IV05 = CodeOpenOrtho(
+IV05 = OpenOrthoCode(
     code=f"{make_code('IV05')}",
     display='Intraoral, Right Buccal Segment, Centric Relation, With Mirror',
-    synonyms=['IV-05','IO.RB.CR.WM'])
+    synonyms=['IV-05', 'IO.RB.CR.WM'])
 """ Used for ... """
 
-IV06 = CodeOpenOrtho(
+IV06 = OpenOrthoCode(
     code=f"{make_code('IV06')}",
     display='Intraoral, Right Buccal Segment, Centric Relation, With Mirror, But Corrected',
-    synonyms=['IV-06','IO.RB.CR.WM.BC'])
+    synonyms=['IV-06', 'IO.RB.CR.WM.BC'])
 """ Used for ... """
 
-IV07 = CodeOpenOrtho(
+IV07 = OpenOrthoCode(
     code=f"{make_code('IV07')}",
     display='Intraoral, Frontal View, Centric Occlusion',
-    synonyms=['IV-07','IO.FV.CO'])
+    synonyms=['IV-07', 'IO.FV.CO'])
 """ Used for ... """
 
-IV08 = CodeOpenOrtho(
+IV08 = OpenOrthoCode(
     code=f"{make_code('IV08')}",
     display='Intraoral, Frontal View, Centric Relation',
-    synonyms=['IV-08','IO.FV.CR'])
+    synonyms=['IV-08', 'IO.FV.CR'])
 """ Used for ... """
 
-IV09 = CodeOpenOrtho(
+IV09 = OpenOrthoCode(
     code=f"{make_code('IV09')}",
     display='Intraoral, Frontal View, Teeth Apart',
-    synonyms=['IV-09','IO.FV.TA'])
+    synonyms=['IV-09', 'IO.FV.TA'])
 """ Used for ... """
 
-IV10 = CodeOpenOrtho(
+IV10 = OpenOrthoCode(
     code=f"{make_code('IV10')}",
     display='Intraoral, Frontal View, Mouth Open',
-    synonyms=['IV-10','IO.FV.MO'])
+    synonyms=['IV-10', 'IO.FV.MO'])
 """ Used for ... """
 
-IV11 = CodeOpenOrtho(
+IV11 = OpenOrthoCode(
     code=f"{make_code('IV11')}",
     display='Intraoral, Frontal View Inferior (showing depth of bite and overjet), Centric Occlusion',
-    synonyms=['IV-11','IO.FV.IV.CO'])
+    synonyms=['IV-11', 'IO.FV.IV.CO'])
 """ Used for ... """
 
-IV12 = CodeOpenOrtho(
+IV12 = OpenOrthoCode(
     code=f"{make_code('IV12')}",
     display='Intraoral, Frontal View Inferior (showing depth of bite and overjet), Centric Relation',
-    synonyms=['IV-12','IO.FV.IV.CR'])
+    synonyms=['IV-12', 'IO.FV.IV.CR'])
 """ Used for ... """
 
-IV13 = CodeOpenOrtho(
+IV13 = OpenOrthoCode(
     code=f"{make_code('IV13')}",
     display='Intraoral, Frontal View, showing Tongue Thrust',
-    synonyms=['IV-13','IO.FV.TT.NM'])
+    synonyms=['IV-13', 'IO.FV.TT.NM'])
 """ Used for ... """
 
-IV14 = CodeOpenOrtho(
+IV14 = OpenOrthoCode(
     code=f"{make_code('IV14')}",
     display='Intraoral, Right Lateral View, Centric Occlusion, showing Overjet, (Direct View showing overjet from the side)',
-    synonyms=['IV-14','IO.RL.CO.OJ'])
+    synonyms=['IV-14', 'IO.RL.CO.OJ'])
 """ Used for ... """
 
-IV15 = CodeOpenOrtho(
+IV15 = OpenOrthoCode(
     code=f"{make_code('IV15')}",
     display='Intraoral, Right Lateral View, Centric Relation, showing Overjet (Direct View showing overjet from the side)',
-    synonyms=['IV-15','IO.RL.CR.OJ'])
+    synonyms=['IV-15', 'IO.RL.CR.OJ'])
 """ Used for ... """
 
-IV16 = CodeOpenOrtho(
+IV16 = OpenOrthoCode(
     code=f"{make_code('IV16')}",
     display='Intraoral, Left Lateral View, Centric Occlusion, showing Overjet, (Direct View showing overjet from the side)',
-    synonyms=['IV-16','IO.LL.CO.OJ'])
+    synonyms=['IV-16', 'IO.LL.CO.OJ'])
 """ Used for ... """
 
-IV17 = CodeOpenOrtho(
+IV17 = OpenOrthoCode(
     code=f"{make_code('IV17')}",
     display='Intraoral, Left Lateral View, Centric Relation, showing Overjet (Direct View showing overjet from the side)',
-    synonyms=['IV-17','IO.LL.CR.OJ'])
+    synonyms=['IV-17', 'IO.LL.CR.OJ'])
 """ Used for ... """
 
 
-IV18 = CodeOpenOrtho(
+IV18 = OpenOrthoCode(
     code=f"{make_code('IV18')}",
     display='Intraoral, Left Buccal Segment, Centric Occlusion (Direct View)',
-    synonyms=['IV-18','IO.LB.CO'])
+    synonyms=['IV-18', 'IO.LB.CO'])
 """ Used for ... """
 
 
-IV19 = CodeOpenOrtho(
+IV19 = OpenOrthoCode(
     code=f"{make_code('IV19')}",
     display='Intraoral, Left Buccal Segment, Centric Occlusion, With Mirror',
-    synonyms=['IV-19','IO.LB.CO.WM'])
+    synonyms=['IV-19', 'IO.LB.CO.WM'])
 """ Used for ... """
 
 
-IV20 = CodeOpenOrtho(
+IV20 = OpenOrthoCode(
     code=f"{make_code('IV20')}",
     display='Intraoral, Left Buccal Segment, Centric Occlusion, With Mirror, But Corrected',
-    synonyms=['IV-20','IO.LB.CO.WM.BC'])
+    synonyms=['IV-20', 'IO.LB.CO.WM.BC'])
 """ Used for ... """
 
 
-IV21 = CodeOpenOrtho(
+IV21 = OpenOrthoCode(
     code=f"{make_code('IV21')}",
     display='Intraoral, Left Buccal Segment, Centric Relation (Direct View)',
-    synonyms=['IV-21','IO.LB.CR'])
+    synonyms=['IV-21', 'IO.LB.CR'])
 """ Used for ... """
 
 
-IV22 = CodeOpenOrtho(
+IV22 = OpenOrthoCode(
     code=f"{make_code('IV22')}",
     display='Intraoral, Left Buccal Segment, Centric Relation, With Mirror',
-    synonyms=['IV-22','IO.LB.CR.WM'])
+    synonyms=['IV-22', 'IO.LB.CR.WM'])
 """ Used for ... """
 
 
-IV23 = CodeOpenOrtho(
+IV23 = OpenOrthoCode(
     code=f"{make_code('IV23')}",
     display='Intraoral, Left Buccal Segment, Centric Relation, With Mirror, But Corrected',
-    synonyms=['IV-23','IO.LB.CR.WM.BC'])
+    synonyms=['IV-23', 'IO.LB.CR.WM.BC'])
 """ Used for ... """
 
 
-IV24 = CodeOpenOrtho(
+IV24 = OpenOrthoCode(
     code=f"{make_code('IV24')}",
     display='Intraoral, Maxillary, Mouth Open, Occlusal View, With Mirror',
-    synonyms=['IV-24','IO.MX.MO.OV.WM'])
+    synonyms=['IV-24', 'IO.MX.MO.OV.WM'])
 """ Used for ... """
 
 
-IV25 = CodeOpenOrtho(
+IV25 = OpenOrthoCode(
     code=f"{make_code('IV25')}",
     display='Intraoral, Maxillary, Mouth Open, Occlusal View, With Mirror, But Corrected',
-    synonyms=['IV-25','IO.MX.MO.OV.WM.BC'])
+    synonyms=['IV-25', 'IO.MX.MO.OV.WM.BC'])
 """ Used for ... """
 
 
-IV26 = CodeOpenOrtho(
+IV26 = OpenOrthoCode(
     code=f"{make_code('IV26')}",
     display='Intraoral, Mandibular, Mouth Open, Occlusal View, With Mirror',
-    synonyms=['IV-26','IO.MD.MO.OV.WM'])
+    synonyms=['IV-26', 'IO.MD.MO.OV.WM'])
 """ Used for ... """
 
 
-IV27 = CodeOpenOrtho(
+IV27 = OpenOrthoCode(
     code=f"{make_code('IV27')}",
     display='Intraoral, Mandibular, Mouth Open, Occlusal View, With Mirror, But Corrected',
-    synonyms=['IV-27','IO.MD.MO.OV.WM.BC'])
+    synonyms=['IV-27', 'IO.MD.MO.OV.WM.BC'])
 """ Used for ... """
 
 
-IV28 = CodeOpenOrtho(
+IV28 = OpenOrthoCode(
     code=f"{make_code('IV28')}",
     display='Intraoral, showing Gingival Recession (ISO tooth numbers)',
-    synonyms=['IV-28','IO.GR.[tooth number]'])
+    synonyms=['IV-28', 'IO.GR.[tooth number]'])
 """ Used for ... """
 
 
-IV29 = CodeOpenOrtho(
+IV29 = OpenOrthoCode(
     code=f"{make_code('IV29')}",
     display='Intraoral, showing Frenum (ISO tooth numbers)',
-    synonyms=['IV-29','IO.FR.[tooth number]'])
+    synonyms=['IV-29', 'IO.FR.[tooth number]'])
 """ Used for ... """
 
 
-IV30 = CodeOpenOrtho(
+IV30 = OpenOrthoCode(
     code=f"{make_code('IV30')}",
     display='Intraoral, any photo using a photo accessory device (modifiers)',
-    synonyms=['IV-30','IO.[modifier].PA'])
+    synonyms=['IV-30', 'IO.[modifier].PA'])
 """ Used for ... """
 
+
+VS01 = OpenOrthoCode(
+    code=f"{make_code('VS01')}",
+    display='ViewSet 01',
+    synonyms=['VS-01', 'ABO'],
+    views=[[EV01], [EV15], [EV19], [IV25], [None], [IV27], [IV01,IV03],[IV07],[IV18,IV20]]
+)
--- open_ortho_terminology/terminology/snomed.py
+++ open_ortho_terminology/terminology/snomed.py
@@ -8,37 +8,72 @@ from . import Code
 PREFIX = "SCT"
 SYSTEM = "http://snomed.info/sct"
 
-class CodeSnomed(Code):
+class SnomedCode(Code):
 
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
         self.prefix = PREFIX
         self.system = SYSTEM
 
-dental_chair = CodeSnomed(
+dental_chair = SnomedCode(
     code='706356006',
     display='Dental examination/treatment chair')
 
-orthod_treatment_perm_class1 = CodeSnomed(
+orthod_treatment_perm_class1 = SnomedCode(
     code='3891000',
     display='Comprehensive orthodontic treatment, permanent dentition, for class I malocclusion')
 
-ortho_treatment = CodeSnomed(
+ortho_treatment = SnomedCode(
     code='122452007',
     display='Comprehensive orthodontic treatment')
 
-orthodontist = CodeSnomed(
+orthodontist = SnomedCode(
     code='37504001',
     display='Orthodontist')
 
-clinical_staff = CodeSnomed(
+clinical_staff = SnomedCode(
     code='4162009',
     display='Dental assistant')
 
-admin_staff = CodeSnomed(
+admin_staff = SnomedCode(
     code='224608005',
     display='Administrative healthcare staff')
 
-tech_support = CodeSnomed(
+tech_support = SnomedCode(
     code='159324001',
     display='Technical assistant')
+
+EV01 = SnomedCode(
+    code='1306623000',
+    display="Photographic extraoral image of right half of face with lips relaxed and teeth in centric occlusion (record artifact)",
+    synonyms=['EV-01', 'EO.RP.LR.CO'])
+""" Used for ... """
+
+
+EV02 = SnomedCode(
+    code='1306622005',
+    display="Photographic extraoral image of right half of face with lips relaxed and jaws in centric relation (record artifact)",
+    synonyms=['EV-02', 'EO.RP.LR.CR'])
+""" Used for ... """
+
+
+EV15 = SnomedCode(
+    code='1306630006',
+    display="Photographic extraoral image of full face with lips relaxed and teeth in centric occlusion (record artifact)",
+    synonyms=['EV-15', 'EO.FF.LR.CO'])
+""" Used for ... """
+
+
+EV17 = SnomedCode(
+    code='1306622005',
+    display="Photographic extraoral image of full face with lips closed and teeth in centric occlusion (record artifact)",
+    synonyms=['EV-17', 'EO.FF.LC.CO'])
+""" Used for ... """
+
+
+EV19 = SnomedCode(
+    code='1306622005',
+    display="Photographic extraoral image of full face with full smile and teeth in centric occlusion (record artifact)",
+    synonyms=['EV-19', 'EO.FF.FS.CO'])
+""" Used for ... """
+
